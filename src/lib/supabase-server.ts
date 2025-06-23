afde3d8f3429e1a205858c68aa250dcf2345e090
Akash R <109196940+Akash-R-007@users.noreply.github.com>
2025-06-23 16:37:32 +0530

    Delete src directory

--- src/lib/supabase-server.ts
+++
@@ -1,9 +0,0 @@
-// lib/supabase-server.ts
-import { cookies } from 'next/headers';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import type { SupabaseClient } from '@supabase/supabase-js';
-
-export async function getSupabaseServerClient(): Promise<SupabaseClient> {
-  const cookieStore = cookies();
-  return createServerComponentClient({ cookies: () => cookieStore });
-}
